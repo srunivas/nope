3bcc0d52e5c5fe9cf0e3a4b5e5b931ce2918f0b5
srunivas <60532589+srunivas@users.noreply.github.com>
2022-05-20 16:54:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 var cors = require('cors');
-const { response } = require('express');
 const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
 const jsonwebtoken = require('jsonwebtoken');
